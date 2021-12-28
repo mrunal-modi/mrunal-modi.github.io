56023f0cac6a8b193d6bb577557107031555d20a
mmodi <mrunal.modi@gmail.com>
2021-12-28 23:57:41 +1100

    Added Contacts.html from contacts_data.js

---
+++ contacts_data.js
@@ -0,0 +1,34 @@
+var contacts = {
+    "users": [
+      {
+        "userId": 1,
+        "firstName": "Mrunal",
+        "lastName": "Modi",
+        "gender": "Male",
+        "age": 21,
+        "address": {
+            "streetAddress": "Glen Iris",
+            "city": "Melbourne",
+            "state": "VIC",
+            "postalCode": "3146"
+        },
+        "emailAddress": "mrunal.modi@gmail.com",
+        "phoneNumbers": [{ "type": "mobile", "number": "61888888888" }]
+      },
+      {
+        "userId": 2,
+        "firstName": "AreWeIn",
+        "lastName": "App",
+        "gender": "Neutral",
+        "age": 21,
+        "address": {
+            "streetAddress": "Glen Iris",
+            "city": "Melbourne",
+            "state": "VIC",
+            "postalCode": "3146"
+        },
+        "emailAddress": "arewein@arewein.com",
+        "phoneNumbers": [{ "type": "mobile", "number": "61888888888" }]
+      }
+    ]
+  }
---
+++ walk_contacts.js
@@ -0,0 +1,21 @@
+function renderContacts(contacts, container) {
+    const users = contacts.users;
+    const len = users.length;
+    let html = '';
+    for (let i = 0; i < len; i++) {
+        if (users[i]['userId'] == 1) { //Get First User
+            html += '<li class="post">' 
+            + '<h1>' + users[i]['firstName'] + " " + users[i]['lastName'] + '</h1>' 
+            + '<h2>' + "Gender: " + users[i]['gender'] + '</h2>'
+            + '<h2>' + "Age: " + users[i]['age'] + '</h2>'
+            + '<h2>' + "City: " + users[i]['address']['city'] + '</h2>'
+            + '<h2>' + "Email: " + users[i]['emailAddress'] + '</h2>'
+            + '<h2>' + "Mobile: " + users[i]['phoneNumbers'][0]['number'] + '</h2>';
+            }
+        }
+    console.log(html)
+    // TODO: add code to display the html variable inside a ul element with id="data"
+    // Hint: you can use the container parameter's innerHTML property to insert Html tags
+    container.innerHTML = '<ul id = "contacts">' + html + '</ul>';
+  }
+  renderContacts(contacts, document.getElementById('contacts-section'));
